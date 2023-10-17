ed1da8ecf704da09053ec53333317d974a063418
eduaardofranco <eduaardo.franco@gmail.com>
2023-10-17 07:32:16 +0100

    dish controller create fnc

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -1,11 +1,47 @@
 const knex = require('../database/knex')
+const AppError = require('../utils/AppError')
 
 class DishesController {
     async create(request, response) {
         const { name, avatar, price, description, ingredients } = request.body
         const user_id = request.user.id
 
-        return response.json({ name, avatar, price, description, ingredients, user_id })
+        if(!name) {
+            throw new AppError('Name is required')
+        }
+        if(!avatar) {
+            throw new AppError('Avatar is required')
+        }
+        if(!price) {
+            throw new AppError('Price is required')
+        }
+        if(!description) {
+            throw new AppError('Description is required')
+        }
+        if(!ingredients) {
+            throw new AppError('Ingredients is required')
+        }
+        
+        const [dish_id] = await knex('dishes')
+        .insert({
+            name: name,
+            avatar: avatar,
+            price: price,
+            description: description,
+            user_id: user_id
+        })
+
+        const ingredientsList = ingredients.map(ingredient => {
+            return {
+                dish_id,
+                name: ingredient
+            }
+         })
+
+         await knex('ingredients').insert(ingredientsList)
+
+
+        return response.json()
     }
 }
 
